9251cd4c04213535f0c6b727110d0957d2cc937d
alexanderololoev@gmail.com <alexanderololoev@gmail.com>
2024-04-16 23:06:50 +0300

    fullfix

--- Archiver/decode.h
+++ Archiver/decode.h
@@ -4,27 +4,17 @@ std::string decode(std::vector<char> fileSectionData, std::vector<std::pair<char
 		data += std::bitset<8>(i).to_string();
 	}
 	if (zeros) data.resize(data.size() - zeros);
-	std::string encData;
-	int beginBin = 0;
-	while (beginBin != data.size()) {
-		int endBin = beginBin;
-		bool flag = true;
-		while (flag) {
-			std::string buffer;
-			int n = beginBin;
-			endBin++;
-			for (n; n < endBin; n++) {
-				buffer += data[n];
-			}
-			for (auto& it : huffmanCode) {
-				if (buffer == it.second) {
-					flag = false;
-					encData += it.first;
-				}
-			}
-			if (endBin == data.size()) flag = false;
-		}
-		beginBin = endBin;
-	}
+    std::string encData;
+    std::string buffer;
+    for (char c : data) {
+        buffer += c;
+        for (auto& code : huffmanCode) {
+            if (buffer == code.second) {
+                encData += code.first;
+                buffer = "";
+                break;
+            }
+        }
+    }
 	return encData;
 }
